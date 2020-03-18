4bfc7146d1552cbe2252c454c3aa5e8178e5f57e
fangqihao <1398909559@qq.com>
2020-03-18 18:17:21 +0800

    预备课练习2

---
+++ demo2.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+mkdir example
+mv ex01.c example/
+mv example/ex01.c example/example.c
+mkdir practice
+mv ex02.c practice/
+mv practice/ex02.c practice/practice.c
+rm ex03.c
+touch README.md
+cd README.md
+echo '## example/example.c  
+判断是奇数还是偶  
+## practice/practice.c  
+比较两个数大小' > README.md
+git add .
+git commit -m "预备课练习2"
+git push 
